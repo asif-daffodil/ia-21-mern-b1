d772a937b16dd5daa10dbfbe1d5574a4b395aeae
asif-daffodil <abir@dipti.com.bd>
2024-10-03 10:26:45 +0600

    10320241026am

--- d32 d33 Sep-30-2024 - Oct-3-2024/src/Components/Footer/Footer.jsx
+++ d32 d33 Sep-30-2024 - Oct-3-2024/src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <div>
+        <div className="text-center py-5">
             <h2>This is a footer</h2>
         </div>
     );
--- d32 d33 Sep-30-2024 - Oct-3-2024/src/Components/Header/Header.jsx
+++ d32 d33 Sep-30-2024 - Oct-3-2024/src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import HeaderCSS from './Header.module.css';
 const Header = () => {
     return (
         <div>
-            <h2 className={`${HeaderCSS.seminarChole} ${HeaderCSS.seminarBondho}`}>This is a header</h2>
+            <h2 className={`${HeaderCSS.seminarChole} ${HeaderCSS.seminarBondho} text-center py-5 bg-stone-300`}>This is a header</h2>
         </div>
     );
 };
